39fe171480bbcc6cab8d390b09be4db5640000a7
Ella Schleimann-Jensen <ellasj@rackham2.uppmax.uu.se>
2021-04-10 11:51:56 +0200

    Re-organized the code folder + tried to do another DNA assembly

---
+++ code/DNA_assembly/DNA_assembly_try_2/sbatch_DNA_assembly_script_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 03:00:00
+#SBATCH -J DNA_assembly_ellasj
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user ellasj@hotmail.com
+
+# Load modules
+module load bioinfo-tools
+module load soapdenovo
+
+# Commands
+SOAPdenovo-63mer all -s soap.config -o asm -F -R -E -w -u -K 55 -p 8 >> SOAPdenovo.log
