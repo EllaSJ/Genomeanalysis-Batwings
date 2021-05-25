d1883776cca43911b9aec7db6c65fe808db9255d
EllaSJ <78213576+EllaSJ@users.noreply.github.com>
2021-05-25 09:12:25 +0200

    Update DESeq2.R

--- Code/Differential Expression/DESeq2.R
+++ Code/Differential Expression/DESeq2.R
@@ -1,17 +1,23 @@
+# Specify directory
 directory <- "~/Documents/Civilingenjör_molekylär_bioteknik/Ar_4/Genome_Analysis/DESeq2_R/htseq_files/"
 
+# Make a list of the files in the directory and specify conditions
 sampleFiles <- list.files(directory)
 sampleCondition <- c("Forelimb", "Forelimb", "Hindlimb", "Hindlimb")
+
+# Make a table out of the files and conditions
 sampleTable <- data.frame(sampleName=sampleFiles, 
                           fileName=sampleFiles, 
                           condition=sampleCondition)
 
+# Load DESeq2 library and create a DESeq2 dataset from htseq count
 library(DESeq2)
 ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable, 
                                        directory = directory, 
                                        design = ~ condition)
 ddsHTSeq
 
+# Write out results
 dds <- DESeq(ddsHTSeq)
 res <- results(dds)
 res
